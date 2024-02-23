64349abd8be471895e848907a1446037b0a30983
Damian <Drobertson127@gmail.com>
2024-02-22 18:22:50 -0700

    Made rough draft of Face off part of the game.

--- Game Files/feud.py
+++ Game Files/feud.py
@@ -1,2 +1,21 @@
-player1 = input("whats your name")
-print("hi")
+question = "name something associated with vampires ".title()
+anwser = {
+    "twilight": 33, 
+    "blood": 29, 
+    "bloodsucker": 29, 
+    "garlic": 9, 
+    "bat": 7, 
+    "cape": 7,
+    "Dracula": 5,
+    "Fangs": 4,
+    "Halloween": 4
+    }
+
+player1 = input("Who will be player one?")
+player2 = input("Who will be player two?")
+guess = input(question)
+if guess in anwser:
+    points = anwser[guess]
+    print(f"Correct, {guess} is on the board for {points} ponts!.")
+
+print(guess)
