bf36bffd33b8ddc7c0bc3fa6f17a4848eacffc1a
Damian127 <69479768+Damian127@users.noreply.github.com>
2024-03-08 23:49:47 +0000

    Added check guess function

--- Game Files/feud.py
+++ Game Files/feud.py
@@ -1,21 +1,21 @@
 question = "name something associated with vampires ".title()
 anwser = {
-    "twilight": 33, 
-    "blood": 29, 
-    "bloodsucker": 29, 
-    "garlic": 9, 
-    "bat": 7, 
+    "twilight": 33,
+    "blood": 29,
+    "bloodsucker": 29,
+    "garlic": 9,
+    "bat": 7,
     "cape": 7,
     "Dracula": 5,
     "Fangs": 4,
     "Halloween": 4
-    }
+}
 
-player1 = input("Who will be player one?")
-player2 = input("Who will be player two?")
+player1 = input("Who will be player one? ")
+player2 = input("Who will be player two? ")
 guess = input(question)
 if guess in anwser:
-    points = anwser[guess]
-    print(f"Correct, {guess} is on the board for {points} ponts!.")
+  points = anwser[guess]
+  print(f"Correct, {guess} is on the board for {points} ponts!.")
 
 print(guess)
