2ecbe1826c5af10f914c8f37fac7511d8858dce7
Damian127 <69479768+Damian127@users.noreply.github.com>
2024-03-11 04:00:54 +0000

    Need to update game list function

--- feud.py
+++ feud.py
@@ -1,3 +1,14 @@
+import os
+from random import randint, choice
+from questions import *
+import random
+def clear():
+    if os.name == 'nt':
+        _ = os.system('cls')
+    else:
+        _ = os.system('clear')
+
+# Rest of the existing code remains unchanged
 def check_guess(guess, answer_list, buzzer, game_board):
   """Checks if player guess is in the answer list and returns how many points gained if any"""
   
@@ -9,38 +20,45 @@ def check_guess(guess, answer_list, buzzer, game_board):
     index = list(answer_list.keys()).index(guess)
     index_num = index + 1
     game_board[str(index + 1)] = guess
+    clear()
     print(f"{player1} guessed the number {index_num} answer ")
     return buzzer
   elif guess not in answer_list:
 
     buzzer += 1
-    print(f"Wrong you got {buzzer}")
+    clear()
+    print(f"Survey says: [X] {guess} is not on the board")
   return buzzer
 
 
 def score_card():
-  print(f"{player1}: points: {score} X's: {buzzer}")
+  print(f"{player1}: points: {score} [X]'s: {buzzer}")
   print(game_board)
 
 
 def add_points(guess):
   points = answer_list[guess]
+  print(f"Survey says {guess} is worth {points}")
   return points
+def guess(answer_list):
+  pass
 
 
+def get_question():
+  question = game_questions[randint(0,2)]
+  for key in question:
+    question1 = key
+    print(question1)
+  answer_list1 = (question[key])
+  return  question1
 
-question = "name something associated with vampires ".title()
-answer_list = {
-    "twilight": 33,
-    "blood": 29,
-    "bloodsucker": 29,
-    "garlic": 9,
-    "bat": 7,
-    "cape": 7,
-    "dracula": 5,
-    "fangs": 4,
-    "halloween": 4
-}
+def get_answer_list():
+  pass
+  game_questions 
+  return  answer_list1
+
+question =question3
+answer_list = answer_list3
 answer_list_length = len(answer_list)
 
 
@@ -55,22 +73,25 @@ def game():
   global end_of_game
   global score 
   global buzzer 
+  print(logo)
   print(
-      f"Round one with {answer_list_length} answers on the board \n{game_board} the question is: "
+    f"Round one with {answer_list_length} answers on the board \n{game_board} the question is: "
   )
-  
+
   while not end_of_game:
-  
+
     guess = input(question)
     buzzer = check_guess(guess, answer_list, buzzer, game_board)
     if guess in answer_list:
       score += add_points(guess)
     score_card()
-    if buzzer == 3:
+    if buzzer == 3 or score == 100:
       end_of_game = True
+      clear()
   
   print(f"GAME OVER you ended with {score} points ")
   print(answer_list)
+
 
 
 game()
---
+++ questions.py
@@ -0,0 +1,46 @@
+question1 = "name something associated with vampires "
+answer_list1= {
+    "twilight": 33,
+    "blood": 29,
+    "bloodsucker": 29,
+    "garlic": 9,
+    "bat": 7,
+    "cape": 7,
+    "dracula": 5,
+    "fangs": 4,
+    "halloween": 4
+}
+question2 = "Name a household pet you would find in a white person house "
+answer_list2 = {"dog": 20,
+                "cat": 15,
+                "hamster": 25,
+                "bunny": 35,
+                "snake": 5
+                
+                
+}
+question3 = "What clubs would white people join in highschool "
+answer_list3 = {
+  "choir": 24,
+  "drama": 16,
+  "chess": 12,
+  "yearbook": 14,
+  "debate" : 16,
+  "math" : 8
+}
+
+game_questions = [{question1 : answer_list1},
+             {question2 : answer_list2},
+             {question3 : answer_list3}
+]
+
+logo = """
+
+ _______  ___           __      ______  __   ___       _______  _______ ____  ____ ________   
+|   _  "\|"  |         /""\    /" _  "\|/"| /  ")     /"     "|/"     "("  _||_ " |"      "\  
+(. |_)  :||  |        /    \  (: ( \___(: |/   /     (: ______(: ______|   (  ) : (.  ___  :) 
+|:     \/|:  |       /' /\  \  \/ \    |    __/       \/    |  \/    | (:  |  | . |: \   ) || 
+(|  _  \\ \  |___   //  __'  \ //  \ _ (// _  \       // ___)  // ___)_ \\ \__/ //(| (___\ || 
+|: |_)  :( \_|:  \ /   /  \\  (:   _) \|: | \  \     (:  (    (:      "|/\\ __ //\|:       :) 
+(_______/ \_______(___/    \___\_______(__|  \__)     \__/     \_______(__________(________/                                                                                                   
+"""
